04e757dcaab4fc8c9c71b1964edd6f0c27d90f96
Lior Kashi <liorka2@mta.ac.il>
2025-04-14 14:20:20 +0300

    Updated2 liorkashi.java

--- liorkashi.java
+++ liorkashi.java
@@ -1,7 +1,7 @@
 public class liorkashi {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-            System.out.println("liorkashi...UPDATED");
+            System.out.println("liorkashi...UPDATED2");
         }
     }
 }
